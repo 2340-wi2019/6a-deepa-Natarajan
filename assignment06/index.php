0f2f6a4a1e91d2563fe2cbefdd41262a49414206
deepa-Natarajan <53449985+deepa-Natarajan@users.noreply.github.com>
2020-01-26 01:43:00 -0600

    Create index.php

---
+++ assignment06/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Assignment 06</title>
+<meta charset="utf-8">
+</head>
+<body>
+<!--Deepa NAtarajan
+    assignment06.php
+    19WI_INFO_2340_WW - Online - Internet Scripting
+    Instructor: Ms.Lisa Thoendel
+    -->
+
+<h1>Hello World</h1>
+<div> Allow me to introduce myself! My name is<?php echo " Deepa Natarajan!"; ?></div>
+<div>I am <?php echo "30" ;?> years old and I am a student at Metropolitan Community College.</div>
+<div>I am currently studying <?php echo "Computer Technology and Management Information Systems"; ?> and I hope to graduate in <?php echo"2020"; ?>.</div>
+
+
+
+
+</body>
+</html>
